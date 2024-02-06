d668c2c2f3512fbe990ebfc91e2a757438084b92
phil1@stanford.edu <phil1@sdf-login01.slac.stanford.edu>
2024-02-06 15:24:42 -0800

    Fixed typo

--- python/gravitree.py
+++ python/gravitree.py
@@ -2,7 +2,7 @@ import ctypes
 import numpy as np
 import numpy.ctypeslib as ctypeslib
 
-gravitree_lib = ctypes.cdll.LoadLibrary("/home/users/phil1/code/src/github.com/phil-mansfield/gravitree/python/gravitree_wrapper.so")
+gravitree_lib = ctypes.cdll.LoadLibrary("/sdf/home/p/phil1/code/src/github.com/phil-mansfield/gravitree/python/gravitree_wrapper.so")
 _c_iterative_binding_energy = gravitree_lib.cIterativeBindingEnergy
 _c_iterative_binding_energy.restype = None
 _c_iterative_binding_energy.argtypes = [
@@ -18,18 +18,23 @@ _c_potential_energy = gravitree_lib.cPotentialEnergy
 _c_potential_energy.restype = None
 _c_potential_energy.argtypes = [
     ctypes.c_int,
-    ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTINUOUS"),
+    ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
     ctypes.c_double,
     ctypes.c_double,
-    ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTINUOUS"),
+    ctypeslib.ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
 ]
 
-def binding_energy(x, v, mp, eps, n_iter=1):
+def binding_energy(x, v, mp, eps, n_iter=1, ok=None):
+    if ok is not None: x, v = x[ok], v[ok]
     n = len(x)
     E = np.zeros(n, dtype=np.float64)
     x = np.ascontiguousarray(x.reshape((3*n,)), dtype=np.float64)
     v = np.ascontiguousarray(v.reshape((3*n,)), dtype=np.float64)
     _c_iterative_binding_energy(n, x, v, mp, eps, n_iter, E)
+    if ok is not None:
+        EE = np.zeros(len(ok))
+        EE[ok] = E
+        E = EE
     return E
 
 def potential_energy(x, mp, eps):
@@ -37,6 +42,7 @@ def potential_energy(x, mp, eps):
     E = np.zeros(len(x), dtype=np.float64)
     x = np.ascontiguousarray(x.reshape((3*n,)), dtype=np.float64)
     _c_potential_energy(n, x, mp, eps, E)
+    return E
 
 def test():
     x = np.array([
