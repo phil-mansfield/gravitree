a54e55ad03edecade6015b8e5af12f711aa85cac
Jay <jaymariebaptista@gmail.com>
2026-04-14 15:30:48 -0700

    Add spline kernel option for evaluating potentials

--- potential.go
+++ potential.go
@@ -63,6 +63,71 @@ func (phi Potential) Approximate(t1, t2 *Tree, i1, i2 int) {
 	}
 }
 
+func (phi Potential) OneSidedLeafSpline(t1, t2 *Tree, i1, i2 int) {
+	node_i2, node_i1 := &t2.Nodes[i2], &t1.Nodes[i1]
+	for i2 := node_i2.Start; i2 < node_i2.End; i2++ {
+		x_i2, idx_i2 := &t2.Points[i2], t2.Index[i2]
+		for i1 := node_i1.Start; i1 < node_i1.End; i1++ {
+			x_i1 := &t1.Points[i1]
+
+			dx := x_i1[0] - x_i2[0]
+			dy := x_i1[1] - x_i2[1]
+			dz := x_i1[2] - x_i2[2]
+			dx2 := dx*dx + dy*dy + dz*dz
+			phi[idx_i2] += pointPotentialSpline(dx2, t1.eps2)
+		}
+	}
+}
+
+func (phi Potential) TwoSidedLeafSpline(t *Tree, i int) {
+	node := &t.Nodes[i]
+	for i := node.Start; i < node.End; i++ {
+		xi, idxi := &t.Points[i], t.Index[i]
+		for j := i + 1; j < node.End; j++ {
+			xj, idxj := &t.Points[j], t.Index[j]
+
+			dx := xj[0] - xi[0]
+			dy := xj[1] - xi[1]
+			dz := xj[2] - xi[2]
+			dx2 := dx*dx + dy*dy + dz*dz
+
+			phiij := pointPotentialSpline(dx2, t.eps2)
+			phi[idxi] += phiij
+			phi[idxj] += phiij
+		}
+	}
+}
+
+func (phi Potential) ApproximateSpline(t1, t2 *Tree, i1, i2 int) {
+	// writes the approximated potential for nodes in
+	// t2 from nodes in t1
+	switch t2.Order {
+	case Monopole:
+		// Loops over the t2 nodes, calculating the
+		// contributions from the t1 nodes.
+		node_i2 := &t2.Nodes[i2]
+		node_i1 := &t1.Nodes[i1]
+
+		x_i1 := &node_i1.Center
+		mass_i1 := float64(node_i1.End - node_i1.Start)
+
+		for i2 := node_i2.Start; i2 < node_i2.End; i2++ {
+			x_i2, idx_i2 := &t2.Points[i2], t2.Index[i2]
+
+			dx := x_i1[0] - x_i2[0]
+			dy := x_i1[1] - x_i2[1]
+			dz := x_i1[2] - x_i2[2]
+			dx2 := dx*dx + dy*dy + dz*dz
+			phi[idx_i2] += pointPotentialSpline(dx2, t1.eps2) * mass_i1
+		}
+	case Quadrupole:
+		// TODO
+		panic("NYI")
+	default:
+		panic(fmt.Sprintf("Unrecognized approximation order code, %d", t2.Order))
+	}
+}
+
 func (phi Potential) OneSidedLeaf(t1, t2 *Tree, i1, i2 int) {
 	node_i2, node_i1 := &t2.Nodes[i2], &t1.Nodes[i1]
 
@@ -122,4 +187,32 @@ func dist2(x1, x2 [3]float64) float64 {
 
 func pointPotential(r2, eps2 float64) float64 {
 	return -1.0 / math.Sqrt(r2+eps2)
+}
+
+func pointPotentialSpline(r2, eps2 float64) float64 {
+	// The spline kernel is a point convolved with a kernel W (see https://arxiv.org/pdf/astro-ph/0505010)
+	// Follows FIRE utils implementation (https://bitbucket.org/awetzel/utilities/src/master/utilities/particle.py)
+
+	r := math.Sqrt(r2)
+	q := r / math.Sqrt(eps2)
+
+	var val float64
+	if q < 0.5 {
+		q2 := q * q
+		q3 := q2 * q
+		q5 := q3 * q2
+		q6 := q5 * q
+		val = 14.0/5.0*q - 16.0/3.0*q3 + 48.0/5.0*q5 - 32.0/5.0*q6
+	} else if q < 1.0 {
+		q2 := q * q
+		q3 := q2 * q
+		q4 := q3 * q
+		q5 := q4 * q
+		q6 := q5 * q
+		val = -1.0/15.0 + 16.0/5.0*q - 32.0/3.0*q3 + 16.0*q4 - 48.0/5.0*q5 + 32.0/15.0*q6
+	} else {
+		val = 1.0
+	}
+
+	return -val / (q * math.Sqrt(eps2))
 }
